f1b9844c64eb6d9accc0a82f6fdd67ed04297cc7
Kutay <95723369+KutayDemirel@users.noreply.github.com>
2021-12-25 02:29:49 +0300

    Update main.c

--- SerialAddition/main.c
+++ SerialAddition/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int baslangic, bitis, sayac;
+    int toplam =0;
+
+    printf("Baslangic sayisi giriniz:\n");
+    scanf("%d",&baslangic);
+
+    printf("Bitis sayisi giriniz:\n");
+    scanf("%d",&bitis);
+
+    if(bitis<baslangic)
+        printf("Hatali sayi giriniz");
+    else{
+        for(sayac=baslangic; sayac <= bitis; sayac++){
+        toplam = toplam + sayac;
+    }
+        printf("Toplam: %d",toplam);
+    }
+return 0;
+}
 
