1dee09af9b61859633e7c9be0d37c03b85b995b6
Kutay <95723369+KutayDemirel@users.noreply.github.com>
2021-12-25 02:35:10 +0300

    Create main.c

---
+++ Factorial/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ 
+  int i, num;
+  unsigned long long fact=1LL;
+
+        printf("Bir sayi giriniz");
+        scanf("%d",&num);
+
+    for(i=2;i<=num;i++){
+        fact = fact * i;
+    }
+        printf("%d sayisinin faktoriyeli : %llu'dir.",num, fact);
+return 0
+}
