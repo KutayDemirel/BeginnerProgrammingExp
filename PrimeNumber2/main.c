4e8556cdddfdb0e770a3264640479ea78d59b450
Kutay <95723369+KutayDemirel@users.noreply.github.com>
2021-12-25 03:32:39 +0300

    Create main.c

---
+++ PrimeNumber2/main.c
@@ -0,0 +1,16 @@
+        int num,i =2;
+
+        printf("Please Enter a number   :");
+        scanf("%d",&num);
+        for(i;i<sayi;i++){
+            if(sayi%i==0){
+                break;
+            }}
+        printf("Number %d",i);
+        if(sayi ==i)
+            printf(" is Prime Number");
+        else
+            printf(" is not Prime Number");
+
+    return 0;
+}
